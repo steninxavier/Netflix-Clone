f3c2d7bf07ba490ee623bd8050f5cdb0d2c8bd8f
steninxavier <steninxavier@gmail.com>
2021-10-10 01:52:03 +0200

    search bar added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,14 @@ export default function NavBar() {
     return (
         <div className="navbar">
             <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="NetflixLogo"/>
+            <form className="form" role="search">
+            <input type="search" id="query" name="q"
+             placeholder="Search movies"
+             aria-label="Search through site content"/>
+             <button>Search</button>
+             </form>
+     
+   
             <img className="avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Avatar"/>
         </div>
     )
