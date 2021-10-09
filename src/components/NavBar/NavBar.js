b90ff6b97c6546897ea5b2c694ebc464f4b3472a
steninxavier <steninxavier@gmail.com>
2021-10-10 01:18:12 +0200

    avatar image updated

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ export default function NavBar() {
     return (
         <div className="navbar">
             <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="NetflixLogo"/>
-            <img className="avatar" src="https://avatars.dicebear.com/v2/identicon/4278ba32f6c5cdab6bd01738efe4bfd4.svg" alt="Avatar"/>
+            <img className="avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Avatar"/>
         </div>
     )
 }
